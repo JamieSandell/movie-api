f99ce0e6cc1c6bbba783f2390d97981054392793
Jamie Sandell <jamie.sandell@outlook.com>
2025-03-24 14:49:23 +0000

    Repositories
    Created the Repositories folder and a repository interface and class for Movie.
    Added a repository consideration (generic) to README.md.

---
+++ backend/Repositories/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IMovieRepository.cs" company="Jamie Sandell">
+// Copyright (c) Jamie Sandell. All rights reserved.
+// </copyright>
+
+namespace Backend.Repositories
+{
+    using Backend.Entities;
+
+    /// <summary>
+    /// Interface for the movie repository.
+    /// </summary>
+    public interface IMovieRepository
+    {
+        /// <summary>
+        /// Get all the movies asynchronously.
+        /// </summary>
+        /// <returns>A list of all the movies</returns>
+        Task<List<Movie>> GetAllMoviesAsync();
+    }
+}
---
+++ backend/Repositories/MovieRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Repositories
+{
+    using Backend.Entities;
+
+    /// <summary>
+    /// The movie repository.
+    /// </summary>
+    public class MovieRepository : IMovieRepository
+    {
+        /// <inheritdoc/>
+        Task<List<Movie>> IMovieRepository.GetAllMoviesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
