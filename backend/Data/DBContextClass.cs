f5f36505c149852cc67e32ab978ae950407532bb
Jamie Sandell <jamie.sandell@outlook.com>
2025-03-24 14:14:40 +0000

    Data
    Created the data folder. Created the DBContextClass within the data folder.

---
+++ backend/Data/DBContextClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Backend.Data
+{
+    using Backend.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Database connection to the data.
+    /// </summary>
+    /// <param name="configuration">Connection config.</param>
+    public class DBContextClass(IConfiguration configuration) : DbContext
+    {
+        private readonly IConfiguration configuration = configuration;
+
+        /// <summary>
+        /// Gets or sets the movies.
+        /// </summary>
+        public DbSet<Movie> Movies { get; set; }
+
+        /// <summary>
+        /// Configure the database for this context.
+        /// </summary>
+        /// <param name="optionsBuilder">The config.</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
