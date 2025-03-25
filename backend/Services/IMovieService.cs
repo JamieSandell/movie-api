9c2f174107cdb8766347b77eab4d1e640282c905
Jamie Sandell <jamie.sandell@outlook.com>
2025-03-25 14:47:03 +0000

    Services
    Added genre as a filter.

--- backend/Services/IMovieService.cs
+++ backend/Services/IMovieService.cs
@@ -16,15 +16,17 @@ namespace Backend.Services
         /// </summary>
         /// <param name="pageNumber">Page number to start on.</param>
         /// <param name="pageSize">Number of items to display per page.</param>
+        /// <param name="genre">Genre to filter on.</param>
         /// <returns>A list of all the movies.</returns>
-        public Task<List<Movie>> GetAllMoviesAsync(int pageNumber, int pageSize);
+        public Task<List<Movie>> GetAllMoviesAsync(int pageNumber, int pageSize, string genre);
 
         /// <summary>
         /// Search for a movie by title asynchronously.
         /// </summary>
         /// <param name="title">The title of the movie to search for.</param>
         /// <param name="searchLimit">Limit the number of search results.</param>
+        /// <param name="genre">The genre to filter on, blank string to ignore.</param>
         /// <returns>A list of movies that match the search.</returns>
-        public Task<List<Movie>> SearchMovieByTitleAsync(string title, int searchLimit);
+        public Task<List<Movie>> SearchMovieByTitleAsync(string title, int searchLimit, string genre);
     }
 }
--- backend/Services/MovieService.cs
+++ backend/Services/MovieService.cs
@@ -16,7 +16,7 @@ namespace Backend.Services
 #pragma warning restore SA1009 // Closing parenthesis should be spaced correctly
     {
         /// <inheritdoc/>
-        public async Task<List<Movie>> GetAllMoviesAsync(int pageNumber, int pageSize)
+        public async Task<List<Movie>> GetAllMoviesAsync(int pageNumber, int pageSize, string genre)
         {
             if (pageNumber < 1)
             {
@@ -28,26 +28,47 @@ namespace Backend.Services
                 pageSize = 1;
             }
 
-            return await dbContext.Movies.
+            if (genre == default)
+            {
+                return await dbContext.Movies.
                 AsNoTracking()
                 .OrderBy(p => p.Id)
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
+            }
+
+            return await dbContext.Movies.
+               AsNoTracking()
+               .OrderBy(p => p.Id)
+               .Where(p => p.Genre == genre)
+               .Skip((pageNumber - 1) * pageSize)
+               .Take(pageSize)
+               .ToListAsync();
         }
 
         /// <inheritdoc/>
-        public async Task<List<Movie>> SearchMovieByTitleAsync(string title, int searchLimit)
+        public async Task<List<Movie>> SearchMovieByTitleAsync(string title, int searchLimit, string genre)
         {
             if (searchLimit < 1)
             {
                 searchLimit = 1;
             }
 
-            return await dbContext.Movies.
+            if (genre == default)
+            {
+                return await dbContext.Movies.
                 AsNoTracking()
                 .OrderBy(p => p.Id)
                 .Where(p => p.Title == title)
+                .Take(searchLimit)
+                .ToListAsync();
+            }
+
+            return await dbContext.Movies.
+                AsNoTracking()
+                .OrderBy(p => p.Id)
+                .Where(p => p.Title == title && p.Genre == genre)
                 .Take(searchLimit)
                 .ToListAsync();
         }
