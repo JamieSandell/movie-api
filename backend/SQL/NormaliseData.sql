378d5e88ad5ec5ee279e4615cc03a3b253ddfa32
Jamie Sandell <jamie.sandell@outlook.com>
2025-04-01 21:57:25 +0100

    Normalisation
    Normalisation script complete.

--- backend/SQL/NormaliseData.sql
+++ backend/SQL/NormaliseData.sql
@@ -1,56 +1,65 @@
 USE Movies
 
+SET IDENTITY_INSERT dbo.Movies ON;
+
+INSERT INTO dbo.Movies (
+	Id,
+	ReleaseDate,
+	Title,
+	Overview,
+	Popularity,
+	VoteCount,
+	VoteAverage,
+	OriginalLanguage,
+	PosterURL
+)
+SELECT
+	Id,
+	ReleaseDate,
+	Title,
+	Overview,
+	Popularity,
+	VoteCount,
+	VoteAverage,
+	OriginalLanguage,
+	PosterURL
+FROM dbo.MoviesRaw;
+
+SET IDENTITY_INSERT dbo.Movies OFF;
+
+DROP TABLE IF EXISTS #Normalised;
+
+CREATE TABLE #Normalised (
+	MovieId INT,
+	Title NVARCHAR(MAX),
+	Genre NVARCHAR(MAX)
+);
+
+INSERT INTO #Normalised (
+	MovieId,
+	Title,
+	Genre
+)
+SELECT
+	M.Id MovieId,
+	MR.Title,
+	TRIM(GenreSplit.value) AS Genre
+FROM dbo.MoviesRaw MR
+INNER JOIN dbo.Movies M ON MR.Id = M.Id
+CROSS APPLY STRING_SPLIT(MR.Genre, ',') AS GenreSplit;
+
 INSERT INTO dbo.Genres (
 	[Description]
 )
-SELECT DISTINCT TRIM(GenreSplit.value)
-FROM dbo.MoviesRaw
-CROSS APPLY STRING_SPLIT(Genre, ',') AS GenreSplit;
-
-INSERT INTO dbo.Movies (
-	ReleaseDate,
-	Title,
-	Overview,
-	Popularity,
-	VoteCount,
-	VoteAverage,
-	OriginalLanguage,
-	PosterURL
-)
-SELECT
-	ReleaseDate,
-	Title,
-	Overview,
-	Popularity,
-	VoteCount,
-	VoteAverage,
-	OriginalLanguage,
-	PosterURL
-FROM dbo.MoviesRaw
-
-SELECT
-	Id,
-	Title,
-	TRIM(GenreSplit.value) AS Genre
-FROM dbo.MoviesRaw
-CROSS APPLY STRING_SPLIT(Genre, ',') AS GenreSplit;
+SELECT DISTINCT Genre
+FROM #Normalised;
 
 INSERT INTO dbo.GenreMovie (
 	GenresId,
 	MovieId
 )
-SELECT *
-FROM
-(
-	SELECT DISTINCT Genre
-	FROM (
-		SELECT
-		Id,
-		Title,
-		TRIM(GenreSplit.value) AS Genre
-	FROM dbo.MoviesRaw
-	CROSS APPLY STRING_SPLIT(Genre, ',') AS GenreSplit;
-	) G
-) MR
-INNER JOIN dbo.Movies M ON MR.Title = M.Title
-INNER JOIN dbo.Genres G ON MR.Genre = G.[Description];
+SELECT
+	G.Id,
+	N.MovieId
+FROM #Normalised N
+INNER JOIN Genres G ON N.Genre = G.[Description];
